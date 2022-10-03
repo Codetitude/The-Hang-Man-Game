81bc27d12c72d218fe53680f734169f8231273fa
Maciej Parol <maciejparol333@gmail.com>
2022-10-03 14:14:17 -0400

    phase#1  pages are connected and global variables declared

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
+console.log(alphabetDisplay)
 
+let alphabet = []
+
+let alphabetDisplay = document.querySelectorAll('.alphabetDisplay')
+
+alphabetDisplay.innerHTML = 
+
+const wordBank = [
+  'complex',
+  'towards',
+  'commercial',
+  'storage',
+  'pressure',
+  'Latin',
+  'indeed',
+  'sexual',
+  'thus',
+  'enhance',
+  'equally',
+  'current',
+  'the',
+  'victim',
+  'down',
+  'until',
+  'whole',
+  'analyze',
+  'formula',
+  'circumstance',
+  'foot',
+  'income',
+  'act',
+  'back',
+  'take',
+  'generally',
+  'contest',
+  'imply',
+  'rural',
+  'dark',
+  'devote',
+  'bar',
+  'liberal',
+  'club',
+  'well',
+  'seven',
+  'somewhat',
+  'paper',
+  'about',
+  'brother',
+  'idea',
+  'tendency',
+  'borrow',
+  'downtown',
+  'guilty',
+  'pass'
+]
+
+let gameOver = false
+
+startGame = () => {}
+
+pickLetter = () => {}
