8beb3b6ce72628768d13b09d43f5e567d5c66ca0
Maciej Parol <maciejparol333@gmail.com>
2022-10-06 09:05:38 -0400

    winning and losing functions done

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-guessedLetters = []
+let correctGuess = 0
+
+let alphabetButton = document.createElement('button')
+
+let randomWord = document.createElement('p')
+
+let gameOver = false
+
+let maxWrong = 0 // 6 is losing
+
+let lifeDisplay = document.querySelector('#lifeCounter')
+
+lifeDisplay.append('6')
 
 const alphabet = [
   'A',
@@ -29,21 +41,17 @@ const alphabet = [
   'Z'
 ]
 
-let randomWord = document.createElement('p')
-
-let gameOver = false
-
 let letterBank = [
-  'complex',
-  'towards',
-  'commercial',
-  'storage',
-  'pressure',
-  'Latin',
-  'indeed',
-  'sexual',
-  'thus',
-  'enhance',
+  'cannonball',
+  'ship',
+  'treasure',
+  'booty',
+  'scurvy',
+  'plank',
+  'mutiny',
+  'salt',
+  'parrot',
+  'siren',
   'equal',
   'current',
   'pea',
@@ -84,46 +92,59 @@ let letterBank = [
 
 let newWord = (randomWord.innerText =
   letterBank[Math.floor(Math.random() * letterBank.length)])
-let wordSplit = newWord.split('').join('   ')
-document.querySelector('#word').append(wordSplit)
+let wordSplit = newWord.toUpperCase().split('')
+
+wordSplit.forEach((newLetter) => {
+  let newSpan = document.createElement('span')
+  newSpan.innerText = newLetter
+  document.querySelector('#word').append(newSpan)
+})
 
 console.log(wordSplit)
+
 //functions
 
 alphabet.forEach((letter) => {
   let alphabetButton = document.createElement('button')
   alphabetButton.innerText = letter
-  alphabetButton.addEventListener('click', () => {
-    alphabetButton.style.color = 'red'
-    console.log(letter)
+  alphabetButton.addEventListener('click', (event) => {
+    event.target.disabled = true
+    if (gameOver === false) {
+      const allSpans = document.querySelectorAll('span')
+      for (let i = 0; i < allSpans.length; i++) {
+        if (event.target.innerText === allSpans[i].innerText) {
+          alphabetButton.style.color = 'green'
+          allSpans[i].style.color = 'black'
+          correctGuess++
+          console.log('this is your win condition ' + correctGuess)
+        }
+      }
+
+      //console.log(event.target.innerText)
+      if (wordSplit.includes(alphabetButton.innerText) === false) {
+        maxWrong++
+        console.log('you lose a life!' + maxWrong)
+      }
+
+      triggerWin()
+      triggerLoss()
+    }
   })
 
   document.querySelector('.alphabetDisplay').appendChild(alphabetButton)
 })
 
-compareLetters = () => {}
-//letter.addEventListener('click', (e) => {
-//if (gameover === false) {
-//letter(e.target)
-//
-//}
-//})
-//})
-//}
-
-//makeLetterButton()
-
-//pickLetter = () => {
-//alphabet.forEach((letter) => {
-//letter.addEventListener('click', (e) => {
-if (gameOver === false) {
+triggerWin = () => {
+  if (correctGuess === wordSplit.length) {
+    gameOver = true
+    alert('Not bad for a scallyway you win!')
+    return
+  }
 }
-//})
-//})
-//}
-//pickLetter()
-console.log(alphabet)
-
-//startGame = () => {}
-
-//startGame()
+triggerLoss = () => {
+  if (maxWrong === 6) {
+    gameOver = true
+    alert('Dance the hempen jig , you loss!')
+    return
+  }
+}
